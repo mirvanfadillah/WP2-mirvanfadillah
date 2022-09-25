d902397eb0a1b852a2f9641c6268f307c01e2a37
mirvanfadillah <fadillahirvan008@gmail.com>
2022-09-25 23:09:47 +0700

    menambahkan

--- application/models/Model-Latihan1.php
+++ application/models/Model-Latihan1.php
@@ -1,10 +1,12 @@
 <?php
  class Model_latihan1 extends CI_Model 
  { 
+ 
     //membuat variable untuk menampung nilai
      public $nilai1, $nilai2, $hasil;
      //method penjumlahan
-public function jumlah($n1 = null, $n2 = null) 
+
+     public function jumlah($n1 = null, $n2 = null)
 
     { 
     
