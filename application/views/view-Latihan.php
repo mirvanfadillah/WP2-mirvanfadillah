56aa58a641a5be7390dda65f8296c2be47c2a9ee
mirvanfadillah <fadillahirvan008@gmail.com>
2022-09-25 23:15:35 +0700

    frist commit

---
+++ application/views/view-Latihan.php
@@ -0,0 +1,13 @@
+<html>
+
+<head>
+    <title>Latihan 1</title>
+</head>
+
+<body>
+    Halo Kawan.. Yuk Kita belajar WEB PROGRAMING..!!<br>
+    nilai 1 = <?= $nilai1; ?>
+    nilai 2 = <?= $nilai2;?>
+    ini  hasil dari pemodelan  dengan method penjumlahan yaitu <?= $nilai ." + " . $nilai2 . " + " . $hasil; ?>
+</body>
+</html>
